0881e32be14cbc9984454bbe36edc1cc0bd614b4
Calplus <76245961+Calplus@users.noreply.github.com>
2026-03-03 02:23:12 +0800

    Revert "frontend: create auth, connect with backend"
    This reverts commit 990051d15bca0c3f3b5d76c09c6b76b316afe628.

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,14 +1,11 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { IncidentCredibilityProvider } from './providers/IncidentCredibilityProvider';
-import { AuthProvider } from './providers/AuthProvider';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <IncidentCredibilityProvider>
-        <RouterProvider router={router} />
-      </IncidentCredibilityProvider>
-    </AuthProvider>
+    <IncidentCredibilityProvider>
+      <RouterProvider router={router} />
+    </IncidentCredibilityProvider>
   );
 }
